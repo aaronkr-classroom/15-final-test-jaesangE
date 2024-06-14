5626d6e13efe3c4de2f3719ca938ce621c94e3ed
jaesangE <gnwldzm@gmail.com>
2024-06-14 10:20:57 +0900

    submit

---
+++ Finaltest/Student_info.cpp
@@ -0,0 +1,42 @@
+//Student_info.cpp
+
+#include <iostream>
+
+#include "Core.h"
+#include "Student_info.h"
+#include "Grad.h"
+#include "Intl.h"
+using std::istream;
+
+istream& Student_info::read(istream& is) {
+	delete cp;
+
+	char ch;
+	is >> ch;
+
+	if (ch == 'U') {
+		cp = new Core(is);
+	}
+	else if(ch =='G'){
+		cp = new Grad(is);
+	}
+	else if (ch == 'I') {
+		cp = new Intl(is);
+	}
+	return is;
+}
+
+Student_info::Student_info(const Student_info& s) : cp(0) {
+	if (s.cp) cp = s.cp->clone();
+}
+
+Student_info& Student_info::operator=(const Student_info& s) {
+	if (&s != this) {
+		delete cp;
+		if (s.cp) cp = s.cp->clone();
+		else cp = 0;
+	}
+
+	return *this;
+	
+}
